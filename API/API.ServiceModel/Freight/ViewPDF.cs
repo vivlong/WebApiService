6dd8e3bf3ba00f779c68a0d57364cc174940b32f
vivlong <vivlonglz@gmail.com>
2016-02-02 11:07:14 +0800

    Add Freight/ViewPDF.cs

---
+++ API/API.ServiceModel/Freight/ViewPDF.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceStack;
+using ServiceStack.ServiceHost;
+
+namespace WebApi.ServiceModel.Freight
+{
+				[Route("/freight/view/pdf", "Get")] // pdf?TableName= & TrxNo= 
+				public class ViewPDF : IReturn<CommonResponse>
+				{
+								public string TableName { get; set; }
+								public string TrxNo { get; set; }
+				}
+}
