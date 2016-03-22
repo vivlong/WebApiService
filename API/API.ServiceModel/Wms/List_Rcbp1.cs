aefb38caa88c2ff929d814c463ec8c0c8651ec89
vivlong <vivlonglz@gmail.com>
2016-03-22 17:18:48 +0800

    Update Wms/List_Rcbp1.cs

--- API/API.ServiceModel/Wms/List_Rcbp1.cs
+++ API/API.ServiceModel/Wms/List_Rcbp1.cs
@@ -31,7 +31,7 @@ namespace WebApi.ServiceModel.Wms
                 {
                     if (!string.IsNullOrEmpty(request.BusinessPartyName))
                     {
-																								string strSQL = "Select *,(Select Top 1 CountryName From Rccy1 Where CountryCode=Rcbp1.CountryCode) AS CountryName From Rcbp1 Where IsNUll(StatusCode,'')<>'DEL' And BusinessPartyName LIKE '" + request.BusinessPartyName + "%' Order By BusinessPartyCode Asc";
+																								string strSQL = "Select *,(Select Top 1 CountryName From Rccy1 Where CountryCode=Rcbp1.CountryCode) AS CountryName From Rcbp1 Where IsNUll(StatusCode,'')<>'DEL' And PartyType='CL' And BusinessPartyName LIKE '" + request.BusinessPartyName + "%' Order By BusinessPartyCode Asc";
 																								Result = db.Select<Rcbp1>(strSQL);
                     }
                     else if (!string.IsNullOrEmpty(request.TrxNo))
@@ -41,7 +41,7 @@ namespace WebApi.ServiceModel.Wms
                     }
                     else
                     {
-																								string strSQL = "Select Top 20 *,(Select Top 1 CountryName From Rccy1 Where CountryCode=Rcbp1.CountryCode) AS CountryName From Rcbp1 Where IsNUll(StatusCode,'')<>'DEL' Order By BusinessPartyName Asc";
+																								string strSQL = "Select Top 20 *,(Select Top 1 CountryName From Rccy1 Where CountryCode=Rcbp1.CountryCode) AS CountryName From Rcbp1 Where IsNUll(StatusCode,'')<>'DEL' And PartyType='CL' Order By BusinessPartyName Asc";
 																								Result = db.Select<Rcbp1>(strSQL);
                     }
                 }
@@ -60,7 +60,7 @@ namespace WebApi.ServiceModel.Wms
 																				string strWhere = "";
 																				if (!string.IsNullOrEmpty(request.BusinessPartyName))
 																				{
-																								strWhere = " Where BusinessPartyName LIKE '" + request.BusinessPartyName + "%'";
+																								strWhere = " Where PartyType='CL' And BusinessPartyName LIKE '" + request.BusinessPartyName + "%'";
 																				}				
 																				string strSelect= "SELECT " +
 																				"r1.*, (Select Top 1 CountryName From Rccy1 Where CountryCode=r1.CountryCode) AS CountryName " +
